1ab97d801ebd9d5feac3c8c6e9218e3e676c3a40
SeungJun9164 <60804222+SeungJun9164@users.noreply.github.com>
2020-08-01 15:41:07 +0900

    Add files via upload

--- BaekJoon_9Level.cpp
+++ BaekJoon_9Level.cpp
@@ -32,28 +32,89 @@
 //	return 0;
 //}
 
-//2839번
+////2839번
+//int main() {
+//	int n, temp;
+//	std::cin >> n;
+//	if (n % 5 == 0)
+//		std::cout << n / 5;
+//	else {
+//		temp = n / 5;
+//		while (temp >= 0) {
+//			if ((n - (temp * 5)) % 3 == 0) {
+//				std::cout << temp + (n - (temp * 5)) / 3;
+//				break;
+//			}
+//			else
+//				temp--;
+//		}
+//		if (temp == -1)
+//			std::cout << -1;
+//	}
+//}
+
+////2292번
+//int main() {
+//	int n, sum = 1, cnt = 1;
+//	std::cin >> n;
+//	while (sum < n) {
+//		sum += cnt * 6;
+//		cnt++;
+//	}
+//	std::cout << cnt;
+//}
+
+////1193번
+//int main() {
+//	int x, cnt = 0, group = 0;
+//	std::cin >> x;
+//	while (cnt < x) {
+//		group++;    
+//		cnt += group; 
+//	}
+//
+//	if (group % 2)
+//		std::cout << ((cnt - x) + 1) << '/' << (group - (cnt - x));
+//	else
+//		std::cout << (group - (cnt - x)) << '/' << ((cnt - x) + 1);
+//}
+
+////2869번
+//int main() {
+//	int v, a, b, day = 1, sum = 0;
+//	std::cin >> a >> b >> v;
+//	while (1) {
+//		sum += a;
+//		if (sum >= v) {
+//			std::cout << day;
+//			break;
+//		}
+//		else {
+//			sum -= b;
+//			day++;
+//		}
+//	}
+//}
+////시간초과 발생
+//int main() {
+//	int v, a, b, day = 0;
+//	std::cin >> a >> b >> v;
+//	if ((v - b) % (a - b) == 0)
+//		std::cout << (v - b) / (a - b);
+//	else
+//		std::cout << ((v - b) / (a - b)) + 1;
+//}
+
+//10250번
 int main() {
-	int n, temp;
-	std::cin >> n;
-	if (n % 5 == 0)
-		std::cout << n / 5;
-	else {
-		temp = n / 5;
-		while (temp >= 0) {
-			if ((n - (temp * 5)) % 3 == 0) {
-				std::cout << temp + (n - (temp * 5)) / 3;
-				break;
-			}
-			else
-				temp--;
+	int t, h, w, n, ho=1, cheung = 1;
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		std::cin >> h >> w >> n;
+		if (n <= h)
+			std::cout << n << "01";
+		else {
+
 		}
-		if (temp == -1)
-			std::cout << -1;
 	}
-}
-
-//2292번
-int main() {
-
 }
