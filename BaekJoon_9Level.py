b23c0710b724b5849921fb3bbacff49c0e0985b7
SeungJun9164 <60804222+SeungJun9164@users.noreply.github.com>
2020-08-03 19:24:49 +0900

    Add files via upload

--- BaekJoon_9Level.py
+++ BaekJoon_9Level.py
@@ -1,66 +1,66 @@
-# # 1712번
-# import sys
-# a, b, c = map(int, sys.stdin.readline().split())
-# if b >= c:
-#     print(-1)
-# else:
-#     cnt = a // (c - b) + 1
-#     print(cnt)
+# 1712번
+import sys
+a, b, c = map(int, sys.stdin.readline().split())
+if b >= c:
+    print(-1)
+else:
+    cnt = a // (c - b) + 1
+    print(cnt)
 
-# # 2839번
-# n = int(input())
-# if n % 5 == 0:
-#     print(n//5)
-# else:
-#     cnt = n // 5
-#     while cnt >= 0:
-#         if (n - (cnt * 5)) % 3 == 0:
-#             print(cnt + (n - (cnt * 5)) // 3)
-#             break
-#         else:
-#             cnt -= 1
-#     if cnt == -1:
-#         print(-1)
+# 2839번
+n = int(input())
+if n % 5 == 0:
+    print(n//5)
+else:
+    cnt = n // 5
+    while cnt >= 0:
+        if (n - (cnt * 5)) % 3 == 0:
+            print(cnt + (n - (cnt * 5)) // 3)
+            break
+        else:
+            cnt -= 1
+    if cnt == -1:
+        print(-1)
 
-# # 2292번
-# n = int(input())
-# sum = 1
-# cnt = 1
-# while n > sum:
-#     sum += cnt * 6
-#     cnt += 1
-# print(cnt)
+# 2292번
+n = int(input())
+sum = 1
+cnt = 1
+while n > sum:
+    sum += cnt * 6
+    cnt += 1
+print(cnt)
 
-# # 1193번
-# x = int(input())
-# cnt = 0
-# group = 0
-# while cnt < x:
-#     group += 1
-#     cnt += group
-# if group % 2 == 0:
-#     print(group - (cnt - x), '/', (cnt - x) + 1, sep='')
-# else:
-#     print((cnt - x) + 1, '/', group - (cnt - x), sep='')
+# 1193번
+x = int(input())
+cnt = 0
+group = 0
+while cnt < x:
+    group += 1
+    cnt += group
+if group % 2 == 0:
+    print(group - (cnt - x), '/', (cnt - x) + 1, sep='')
+else:
+    print((cnt - x) + 1, '/', group - (cnt - x), sep='')
 
-# # 2869번
-# a, b, v = map(int, input().split())
-# if ((v - b) % (a - b)) == 0:
-#     day = (v - b) // (a - b)
-# else:
-#     day = (v - b) // (a - b) + 1
-# print(day)
+# 2869번
+a, b, v = map(int, input().split())
+if ((v - b) % (a - b)) == 0:
+    day = (v - b) // (a - b)
+else:
+    day = (v - b) // (a - b) + 1
+print(day)
 
-# # 10250번
-# t = int(input())
-# for i in range(t):
-#     h, w, n = map(int, input().split())
-#     y = n % h
-#     x = n // h + 1
-#     if y == 0:
-#         y = h
-#         x -= 1
-#     print(y * 100 + x)
+# 10250번
+t = int(input())
+for i in range(t):
+    h, w, n = map(int, input().split())
+    y = n % h
+    x = n // h + 1
+    if y == 0:
+        y = h
+        x -= 1
+    print(y * 100 + x)
 
 # 2775번
 str = [[0 for i in range(15)] for j in range(15)]
@@ -77,3 +77,22 @@ for i in range(t):
     k = int(input())
     n = int(input())
     print(str[k][n - 1])
+
+# 1011번
+t = int(input())
+for i in range(t):
+    x, y = map(int, input().split())
+    dist = y - x
+    k = 0
+    while True:
+        k += 1
+        if (dist - k ** 2) <= 0:
+            break
+    if (dist - k ** 2) == 0:
+        print(2 * k - 1)
+    else:
+        k = k - 1
+        if (dist - k ** 2) <= k:
+            print(2 * k)
+        else:
+            print(2 * k + 1)
