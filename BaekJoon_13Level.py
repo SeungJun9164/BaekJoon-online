ef0155ea7c06638332cf74b97724ae1ef52e74a1
SeungJun9164 <60804222+SeungJun9164@users.noreply.github.com>
2020-08-16 20:33:45 +0900

    Add files via upload

--- BaekJoon_13Level.py
+++ BaekJoon_13Level.py
@@ -1,45 +1,45 @@
 # 브루드 포스 알고리즘(brute force algorithm)
 # 모든 경우의 수를 모두 해보는 것
 
-# # 2798번
-# n, m = map(int, input().split())
-# arr = list(map(int, input().split()))
-# _sum = 0
-# for i in range(0, len(arr) - 2):
-#     for j in range(i + 1, len(arr) - 1):
-#         for k in range(j + 1, len(arr)):
-#             if arr[i] + arr[j] + arr[k] <= m:
-#                 _sum = max(_sum, arr[i] + arr[j] + arr[k])
-#             else:
-#                 continue
-# print(_sum)
-#
-# # 2231번
-# n = int(input())
-# for i in range(1, n+1):
-#     x = list(map(int, str(i)))
-#     _sum = i + sum(x)
-#     if _sum == n:
-#         print(i)
-#         break
-#     if i == n:
-#         print(0)
+# 2798번
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+_sum = 0
+for i in range(0, len(arr) - 2):
+    for j in range(i + 1, len(arr) - 1):
+        for k in range(j + 1, len(arr)):
+            if arr[i] + arr[j] + arr[k] <= m:
+                _sum = max(_sum, arr[i] + arr[j] + arr[k])
+            else:
+                continue
+print(_sum)
 
-# # 7568번
-# n = int(input())
-# a = []
-# for i in range(n):
-#     x, y = list(map(int, input().split()))
-#     a.append((x, y))
-# arr = []
-# for i in range(len(a)):
-#     rank = 1
-#     for j in range(len(a)):
-#         if a[i][0] < a[j][0] and a[i][1] < a[j][1]:
-#             rank += 1
-#     arr.append(rank)
-# for i in arr:
-#     print(i, end=' ')
+# 2231번
+n = int(input())
+for i in range(1, n+1):
+    x = list(map(int, str(i)))
+    _sum = i + sum(x)
+    if _sum == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
+
+# 7568번
+n = int(input())
+a = []
+for i in range(n):
+    x, y = list(map(int, input().split()))
+    a.append((x, y))
+arr = []
+for i in range(len(a)):
+    rank = 1
+    for j in range(len(a)):
+        if a[i][0] < a[j][0] and a[i][1] < a[j][1]:
+            rank += 1
+    arr.append(rank)
+for i in arr:
+    print(i, end=' ')
 
 # 1018번
 N, M = map(int, input().split())
@@ -69,4 +69,13 @@ for y in range(N-7):
         min_ = min(min_, cnt)
 
 print(min_)
+
+# 1436번
+n = int(input())
+_str = 666
+while n:
+    if '666' in str(_str):
+        n -= 1
+    _str += 1
+print(_str - 1)
 
