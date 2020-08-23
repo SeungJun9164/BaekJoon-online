867849ec6a7cce7244c5a6121565154bd0dca331
SeungJun9164 <60804222+SeungJun9164@users.noreply.github.com>
2020-08-23 13:47:56 +0900

    Add files via upload

--- BaekJoon_15Level.cpp
+++ BaekJoon_15Level.cpp
@@ -1,1 +1,101 @@
 #include<iostream>
+
+// 15649번
+int n, m;
+int arr[9] = { 0 };
+bool chk[9] = { false };
+void dfs(int cnt) {
+
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) 
+			std::cout << arr[i] << ' ';
+		std::cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (chk[i] == false) {
+			arr[cnt] = i;
+			chk[i] = true;
+			dfs(cnt + 1);
+			chk[i] = false;
+
+		}
+	}
+}
+int main() {	
+	std::cin >> n >> m;	
+	dfs(0);
+}
+
+//15650번
+int n, m;
+int arr[9] = { 0 };
+bool chk[9] = { false };
+void dfs(int idx, int cnt) {
+
+	if (cnt == m) {
+		for (int i = 0; i < m; i++)
+			std::cout << arr[i] << ' ';
+		std::cout << '\n';
+		return;
+	}
+	for (int i = idx; i <= n; i++) {
+		if (chk[i] == false) {
+			arr[cnt] = i;
+			chk[i] = true;
+			dfs(i + 1, cnt + 1);
+			chk[i] = false;
+
+		}
+	}
+}
+int main() {
+	std::cin >> n >> m;
+	dfs(1, 0);
+}
+
+//15651번
+int n, m;
+int arr[9] = { 0 };
+bool chk[9] = { false };
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) 
+			std::cout << arr[i] << ' ';
+		std::cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		arr[cnt] = i;
+		chk[i] = true;
+		dfs(cnt + 1);
+		chk[i] = false;
+	}
+}
+int main() {	
+	std::cin >> n >> m;	
+	dfs(0);
+}
+
+//15652번
+int n, m;
+int arr[9] = { 0 };
+bool chk[9] = { false };
+void dfs(int idx, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) 
+			std::cout << arr[i] << ' ';
+		std::cout << '\n';
+		return;
+	}
+	for (int i = idx; i <= n; i++) {
+		arr[cnt] = i;
+		chk[i] = true;
+		dfs(i, cnt + 1);
+		chk[i] = false;
+	}
+}
+int main() {	
+	std::cin >> n >> m;	
+	dfs(1, 0);
+}
